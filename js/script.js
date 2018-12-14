8a8ab63ab4d5dedb1c0c29bc4916d28682a9c99d
Cintia Arambula <cintiaarambula@ccpaedu.com>
2018-12-14 02:26:46 +0000

    update

--- js/script.js
+++ js/script.js
@@ -9,6 +9,55 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
+        var dogPoints;
+        var catPoints;
+        
+        
+        if(name.equals("Dark Colors") || name.equals("dark colors"))
+        {
+            catPoints = catPoints + 1;
+        }
+        else
+        {
+            dogPoints = dogPoints + 1;
+        }
+        
+        if(q1Result.equals("Indoor") || q1Result.equals("dark colors"))
+        {
+            catPoints = catPoints + 1;
+        }
+        else
+        {
+            dogPoints = dogPoints + 1;
+        }
+        
+        if(q2Result.equals("Yes") || q2Result.equals("yes"))
+        {
+            dogPoints = dogPoints + 1;
+        }
+        else if(q2Result.equals("No") || q2Result.equals("no"))
+        {
+            dogPoints = dogPoints - 1;
+            catPoints = catPoints - 1;
+        }
+        else
+        {
+            catPoints = catPoints + 1;
+        }
+        
+        if(q3Result.equals("Friends") || q3Result.equals("friends"))
+        {
+            dogPoints = dogPoints - 1;
+            catPoints = catPoints - 1;
+        }
+        else
+        {
+            catPoints = catPoints + 1;
+            dogPoints = dogPoints + 1; 
+        }
+        
+        $().
     });
 
 });
